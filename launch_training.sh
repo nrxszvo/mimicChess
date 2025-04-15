18c912c4940ff411cfc392b667ce7870919ed5b3
Michael Horgan <michaelghorgan@gmail.com>
2025-04-15 10:24:00 -0700

    update dbg cfg

--- launch_training.sh
+++ launch_training.sh
@@ -7,17 +7,7 @@ node_rank=0
 rdzv_id=456
 rdzv_backend=c10d
 rdzv_endpoint=0.0.0.0:1234
-
-cmd="git diff-index --quiet HEAD --"
-$cmd
-if [ $? -ne 0 ]
-then
-	echo "uncommited changes in working tree; exiting";
-	exit
-fi
-
-commit=$(git rev-parse HEAD)
-echo "commit: ${commit}"
+disable_commit=false
 
 for var in "$@"
 do
@@ -43,11 +33,29 @@ do
 	elif [[ ${param[0]} = "node_rank" ]]
 	then
 		node_rank=${param[1]}
+	elif [[ ${param[0]} = "disable_commit" ]]
+	then
+		disable_commit=${param[1]}
 	else
 		echo "didn't recognize ${var}" 
 		exit
 	fi
 done
+
+if [ "$disable_commit" = false ]
+then
+	cmd="git diff-index --quiet HEAD --"
+	$cmd
+	if [ $? -ne 0 ]
+	then
+		echo "uncommited changes in working tree; exiting";
+		exit
+	fi
+fi
+
+commit=$(git rev-parse HEAD)
+echo "commit: ${commit}"
+
 cmd="OMP_NUM_THREADS=${nthread} NCCL_DEBUG=INFO NCCL_SOCKET_IFNAME=enp230s0 torchrun --nnodes=${nnodes} --node-rank=${node_rank} --nproc-per-node=${nproc} --rdzv-id=${rdzv_id} --rdzv-backend=${rdzv_backend} --rdzv-endpoint=${rdzv_endpoint} train.py --name ${name} --commit ${commit} --cfg ${cfg} --num_nodes ${nnodes}"
 echo $cmd
 eval $cmd
